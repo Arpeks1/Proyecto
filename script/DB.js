bc124b138f18a035dd65c54bd57b544ac1eec07f
Sorli Lisandro <lisandrosorli08@gmail.com>
2026-07-01 23:13:04 -0300

    db(sin las imagenes todavia) y main

--- script/DB.js
+++ script/DB.js
@@ -2,7 +2,7 @@ export const DB=[
     {
         ID:1,
         Nombre:"Pinturas",
-        Precio:"240.899",
+        Precio:"240899",
         Stock:"20",
         Descripcion:"Set De Pintura Y Herramientas Para Miniaturas Warhammer 40k",
         Imagen:""
@@ -10,7 +10,7 @@ export const DB=[
     {
         ID:2,
         Nombre: "Miniatura",
-        Precio:"137.708",
+        Precio:"137708",
         Stock:"12",
         Descripcion:"Warhammer 40k Orkos Flash  Gitz Games Workshop 5 Figuras.",
         Imagen:""
@@ -18,7 +18,7 @@ export const DB=[
     {
         ID:3,
         Nombre: "Miniatura",
-        Precio:"117.719",
+        Precio:"117719",
         Stock:"40",
         Descripcion:"Warhammer 40k: Orkos Batallón Armagedón.",
         Imagen:""
@@ -26,7 +26,7 @@ export const DB=[
     {
         ID:4,
         Nombre: "Figura",
-        Precio:"800.000",
+        Precio:"800000",
         Stock:"20",
         Descripcion:"McFarlane Toys Warhammer 40k Ork Big Mek Mega Figura De Accion Con Accesorio.",
         Imagen:""
@@ -34,15 +34,15 @@ export const DB=[
     {
         ID:5,
         Nombre: "Miniatura",
-        Precio:"158.391",
+        Precio:"158391",
         Stock:"12",
         Descripcion:"Warhammer 40k Ork Warbiker Mob.",
         Imagen:""
     },
     {
         ID:6,
-        Nombre: "Dados",
-        Precio:"40.000",
+        Nombre: "Extra",
+        Precio:"40000",
         Stock:"19",
         Descripcion:"Set De Dados Orkos.",
         Imagen:""
@@ -50,754 +50,353 @@ export const DB=[
     {
         ID:7,
         Nombre: "Miniatura",
-        Precio:"320.000",
+        Precio:"60000",
         Stock:"40",
-        Descripcion:"",
+        Descripcion:"Wazdakka Gutsmek",
         Imagen:""
     },
     {
         ID:8,
-        Nombre: "",
-        Precio:"",
-        Stock:"",
-        Descripcion:"",
+        Nombre: "Libro",
+        Precio:"70000",
+        Stock:"12",
+        Descripcion:"CODEX: ORKS",
         Imagen:""
     },
     {
         ID:9,
-        Nombre: "",
-        Precio:"",
-        Stock:"",
-        Descripcion:"",
+        Nombre: "miniatura",
+        Precio:"67632,08",
+        Stock:"40",
+        Descripcion:"Boyz",
         Imagen:""
     },
     {
         ID:10,
-        Nombre: "",
-        Precio:"",
-        Stock:"",
-        Descripcion:"",
+        Nombre: "Figura",
+        Precio:"51099,80",
+        Stock:"15",
+        Descripcion:"Warboss in mega armour",
         Imagen:""
     },
     {
         ID:11,
-        Nombre: "",
-        Precio:"",
-        Stock:"",
-        Descripcion:"",
+        Nombre: "Miniatura",
+        Precio:"51851,26",
+        Stock:"39",
+        Descripcion:"Jefe matazanoz",
         Imagen:""
     },
     {
         ID:12,
-        Nombre: "",
-        Precio:"",
-        Stock:"",
-        Descripcion:"",
+        Nombre: "miniatura",
+        Precio:"82661,44",
+        Stock:"40",
+        Descripcion:"Kabezabúnker",
         Imagen:""
     },
     {
         ID:13,
-        Nombre: "",
-        Precio:"",
-        Stock:"",
-        Descripcion:"",
+        Nombre: "Figura",
+        Precio:"95853,87",
+        Stock:"52",
+        Descripcion:"Ghazghkull Mag Uruk Thraka",
         Imagen:""
     },
     {
         ID:14,
-        Nombre: "",
-        Precio:"",
-        Stock:"",
-        Descripcion:"",
+        Nombre: "Figura",
+        Precio:"105205,46",
+        Stock:"20",
+        Descripcion:"Big'Ed BossBunka",
         Imagen:""
     },
     {
         ID:15,
-        Nombre: "",
-        Precio:"",
-        Stock:"",
-        Descripcion:"",
+        Nombre: "Figura",
+        Precio:"75146,76",
+        Stock:"23",
+        Descripcion:"Mek Gun",
         Imagen:""
     },
     {
         ID:16,
-        Nombre: "",
-        Precio:"",
-        Stock:"",
-        Descripcion:"",
+        Nombre: "Figura",
+        Precio:"86418,77",
+        Stock:"12",
+        Descripcion:"Ork Meganobz",
         Imagen:""
     },
     {
         ID:17,
-        Nombre: "",
-        Precio:"",
-        Stock:"",
-        Descripcion:"",
+        Nombre: "Figura",
+        Precio:"152547,92",
+        Stock:"63",
+        Descripcion:"Karro de guerra orko",
         Imagen:""
     },
     {
         ID:18,
-        Nombre: "",
-        Precio:"",
-        Stock:"",
-        Descripcion:"",
+        Nombre: "Figura",
+        Precio:"46735,06",
+        Stock:"53",
+        Descripcion:"Lataz Azezinas",
         Imagen:""
     },
     {
         ID:19,
-        Nombre: "",
-        Precio:"",
-        Stock:"",
-        Descripcion:"",
+        Nombre: "Figura",
+        Precio:"71389,42",
+        Stock:"23",
+        Descripcion:"Kamion Orko",
         Imagen:""
     },
     {
         ID:20,
-        Nombre: "",
-        Precio:"",
-        Stock:"",
-        Descripcion:"",
+        Nombre: "Miniatura",
+        Precio:"79346",
+        Stock:"13",
+        Descripcion:"Ork Warbiker Mob",
         Imagen:""
     },
     {
         ID:21,
-        Nombre: "",
-        Precio:"",
-        Stock:"",
-        Descripcion:"",
+        Nombre: "Miniatura",
+        Precio:"52538,44",
+        Stock:"31",
+        Descripcion:"Chikolata del Gobbo Rojo",
         Imagen:""
     },
     {
         ID:22,
-        Nombre: "",
-        Precio:"",
-        Stock:"",
-        Descripcion:"",
+        Nombre: "Miniatura",
+        Precio:"61545,03",
+        Stock:"5",
+        Descripcion:"Mekániko",
         Imagen:""
     },
     {
         ID:23,
-        Nombre: "",
-        Precio:"",
-        Stock:"",
-        Descripcion:"",
+        Nombre: "Miniatura",
+        Precio:"51787,89",
+        Stock:"5",
+        Descripcion:"Jefe Snikrot",
         Imagen:""
     },
     {
         ID:24,
-        Nombre: "",
-        Precio:"",
-        Stock:"",
-        Descripcion:"",
+        Nombre: "Miniatura",
+        Precio:"172626,30",
+        Stock:"13",
+        Descripcion:"Plataforkma",
         Imagen:""
     },
     {
         ID:25,
-        Nombre: "",
-        Precio:"",
-        Stock:"",
-        Descripcion:"",
+        Nombre: "Miniatura",
+        Precio:"75054,91",
+        Stock:"34",
+        Descripcion:"Mozrog Skaragbad",
         Imagen:""
     },
     {
         ID:26,
-        Nombre: "",
-        Precio:"",
-        Stock:"",
-        Descripcion:"",
+        Nombre: "Miniatura",
+        Precio:"75054,91",
+        Stock:"12",
+        Descripcion:"Chikoz Trinkabeztiaz",
         Imagen:""
     },
     {
         ID:27,
-        Nombre: "",
-        Precio:"",
-        Stock:"",
-        Descripcion:"",
+        Nombre: "Miniatura",
+        Precio:"51787,89",
+        Stock:"45",
+        Descripcion:"Kabezilla Orko",
         Imagen:""
     },
     {
         ID:28,
-        Nombre: "",
-        Precio:"",
-        Stock:"",
-        Descripcion:"",
+        Nombre: "Miniatura",
+        Precio:"82560,41",
+        Stock:"35",
+        Descripcion:"Guerreroz en garrapatocino",
         Imagen:""
     },
     {
         ID:29,
-        Nombre: "",
-        Precio:"",
-        Stock:"",
-        Descripcion:"",
+        Nombre: "Miniatura",
+        Precio:"52688,55",
+        Stock:"33",
+        Descripcion:"Zodgrod Verrugozgrod",
         Imagen:""
     },
     {
         ID:30,
-        Nombre: "",
-        Precio:"",
-        Stock:"",
-        Descripcion:"",
+        Nombre: "Miniatura",
+        Precio:"71302,17",
+        Stock:"13",
+        Descripcion:"Shokkjump Dragsta",
         Imagen:""
     },
     {
         ID:31,
-        Nombre: "",
-        Precio:"",
-        Stock:"",
-        Descripcion:"",
+        Nombre: "Miniatura",
+        Precio:"71302,17",
+        Stock:"15",
+        Descripcion:"Orks Rukkatrukk Squigbuggy",
         Imagen:""
     },
     {
         ID:32,
-        Nombre: "",
-        Precio:"",
-        Stock:"",
-        Descripcion:"",
+        Nombre: "Miniatura",
+        Precio:"75054,91",
+        Stock:"28",
+        Descripcion:"Orks Megatrak Scrapjet",
         Imagen:""
     },
     {
         ID:33,
-        Nombre: "",
-        Precio:"",
-        Stock:"",
-        Descripcion:"",
+        Nombre: "Miniatura",
+        Precio:"71302,17",
+        Stock:"2",
+        Descripcion:"Orks Deffkilla Wartrike",
         Imagen:""
     },
     {
         ID:34,
-        Nombre: "",
-        Precio:"",
-        Stock:"",
-        Descripcion:"",
+        Nombre: "Miniatura",
+        Precio:"77306,56",
+        Stock:"24",
+        Descripcion:"Flash Gitz",
         Imagen:""
     },
     {
         ID:35,
-        Nombre: "",
-        Precio:"",
-        Stock:"",
-        Descripcion:"",
+        Nombre: "Miniatura",
+        Precio:"31523,06",
+        Stock:"47",
+        Descripcion:"Ork gretchin",
         Imagen:""
     },
     {
         ID:36,
-        Nombre: "",
-        Precio:"",
-        Stock:"",
-        Descripcion:"",
+        Nombre: "Miniatura",
+        Precio:"51787,89",
+        Stock:"21",
+        Descripcion:"Ork Nobz",
         Imagen:""
     },
     {
         ID:37,
-        Nombre: "",
-        Precio:"",
-        Stock:"",
-        Descripcion:"",
+        Nombre: "Miniatura",
+        Precio:"202648,27",
+        Stock:"10",
+        Descripcion:"batallón de Armageddon: Orkos",
         Imagen:""
     },
     {
         ID:38,
-        Nombre: "",
-        Precio:"",
-        Stock:"",
-        Descripcion:"",
+        Nombre: "Miniatura",
+        Precio:"82560,41",
+        Stock:"48",
+        Descripcion:"Orkomandos",
         Imagen:""
     },
     {
         ID:39,
-        Nombre: "",
-        Precio:"",
-        Stock:"",
-        Descripcion:"",
+        Nombre: "Miniatura",
+        Precio:"55540,64",
+        Stock:"17",
+        Descripcion:"Ork Boyz",
         Imagen:""
     },
     {
         ID:40,
-        Nombre: "",
-        Precio:"",
-        Stock:"",
-        Descripcion:"",
+        Nombre: "Miniatura",
+        Precio:"79224,63",
+        Stock:"27",
+        Descripcion:"Orks Boomdakka Snazzwagon",
         Imagen:""
     },
     {
         ID:41,
-        Nombre: "",
-        Precio:"",
-        Stock:"",
-        Descripcion:"",
+        Nombre: "Miniatura",
+        Precio:"39028,56",
+        Stock:"36",
+        Descripcion:"Ork Paintboy",
         Imagen:""
     },
     {
         ID:42,
-        Nombre: "",
-        Precio:"",
-        Stock:"",
-        Descripcion:"",
+        Nombre: "Miniatura",
+        Precio:"51787,89",
+        Stock:"1",
+        Descripcion:"Ork lootas",
         Imagen:""
     },
     {
         ID:43,
-        Nombre: "",
-        Precio:"",
-        Stock:"",
-        Descripcion:"",
+        Nombre: "Miniatura",
+        Precio:"51787,89",
+        Stock:"30",
+        Descripcion:"Ork Stormboyz",
         Imagen:""
     },
     {
         ID:44,
-        Nombre: "",
-        Precio:"",
-        Stock:"",
-        Descripcion:"",
+        Nombre: "Pinturas",
+        Precio:"79014",
+        Stock:"34",
+        Descripcion:"Set de pinturas para miniaturas y figuras de Warhammer 40k",
         Imagen:""
     },
     {
         ID:45,
-        Nombre: "",
-        Precio:"",
-        Stock:"",
-        Descripcion:"",
+        Nombre: "Ropa",
+        Precio:"30000",
+        Stock:"40",
+        Descripcion:"Remera con el estampado del logo de la pagina",
         Imagen:""
     },
     {
         ID:46,
-        Nombre: "",
-        Precio:"",
-        Stock:"",
-        Descripcion:"",
+        Nombre: "Extra",
+        Precio:"5300",
+        Stock:"23",
+        Descripcion:"llavero del logo de la pagina",
         Imagen:""
     },
     {
         ID:47,
-        Nombre: "",
-        Precio:"",
-        Stock:"",
-        Descripcion:"",
+        Nombre: "Ropa",
+        Precio:"50000",
+        Stock:"20",
+        Descripcion:"Buzo con estampado del logo de la pagina",
         Imagen:""
     },
     {
         ID:48,
-        Nombre: "",
-        Precio:"",
-        Stock:"",
-        Descripcion:"",
+        Nombre: "Ropa",
+        Precio:"75000",
+        Stock:"13",
+        Descripcion:"Campera con estampado del logo de la pagina",
         Imagen:""
     },
     {
         ID:49,
-        Nombre: "",
-        Precio:"",
-        Stock:"",
-        Descripcion:"",
+        Nombre: "Extra",
+        Precio:"30000",
+        Stock:"29",
+        Descripcion:"Mouse pad de 60 x 120 cm con el la pagina",
         Imagen:""
     },
     {
         ID:50,
-        Nombre: "",
-        Precio:"",
-        Stock:"",
-        Descripcion:"",
+        Nombre: "Extra",
+        Precio:"35600",
+        Stock:"20",
+        Descripcion:"Mate tematizado de los ORKOS de Warhammer 40k",
         Imagen:""
-    },
-    {
-        ID:51,
-        Nombre: "",
-        Precio:"",
-        Stock:"",
-        Descripcion:"",
-        Imagen:""
-    },
-    {
-        ID:52,
-        Nombre: "",
-        Precio:"",
-        Stock:"",
-        Descripcion:"",
-        Imagen:""
-    },
-    {
-        ID:53,
-        Nombre: "",
-        Precio:"",
-        Stock:"",
-        Descripcion:"",
-        Imagen:""
-    },
-    {
-        ID:54,
-        Nombre: "",
-        Precio:"",
-        Stock:"",
-        Descripcion:"",
-        Imagen:""
-    },
-    {
-        ID:55,
-        Nombre: "",
-        Precio:"",
-        Stock:"",
-        Descripcion:"",
-        Imagen:""
-    },
-    {
-        ID:56,
-        Nombre: "",
-        Precio:"",
-        Stock:"",
-        Descripcion:"",
-        Imagen:""
-    },
-    {
-        ID:57,
-        Nombre: "",
-        Precio:"",
-        Stock:"",
-        Descripcion:"",
-        Imagen:""
-    },
-    {
-        ID:58,
-        Nombre: "",
-        Precio:"",
-        Stock:"",
-        Descripcion:"",
-        Imagen:""
-    },
-    {
-        ID:59,
-        Nombre: "",
-        Precio:"",
-        Stock:"",
-        Descripcion:"",
-        Imagen:""
-    },
-    {
-        ID:60,
-        Nombre: "",
-        Precio:"",
-        Stock:"",
-        Descripcion:"",
-        Imagen:""
-    },
-    {
-        ID:61,
-        Nombre: "",
-        Precio:"",
-        Stock:"",
-        Descripcion:"",
-        Imagen:""
-    },
-    {
-        ID:62,
-        Nombre: "",
-        Precio:"",
-        Stock:"",
-        Descripcion:"",
-        Imagen:""
-    },
-    {
-        ID:63,
-        Nombre: "",
-        Precio:"",
-        Stock:"",
-        Descripcion:"",
-        Imagen:""
-    },
-    {
-        ID:64,
-        Nombre: "",
-        Precio:"",
-        Stock:"",
-        Descripcion:"",
-        Imagen:""
-    },
-    {
-        ID:65,
-        Nombre: "",
-        Precio:"",
-        Stock:"",
-        Descripcion:"",
-        Imagen:""
-    },
-    {
-        ID:66,
-        Nombre: "",
-        Precio:"",
-        Stock:"",
-        Descripcion:"",
-        Imagen:""
-    },
-    {
-        ID:67,
-        Nombre: "",
-        Precio:"",
-        Stock:"",
-        Descripcion:"",
-        Imagen:""
-    },
-    {
-        ID:68,
-        Nombre: "",
-        Precio:"",
-        Stock:"",
-        Descripcion:"",
-        Imagen:""
-    },
-    {
-        ID:69,
-        Nombre: "",
-        Precio:"",
-        Stock:"",
-        Descripcion:"",
-        Imagen:""
-    },
-    {
-        ID:70,
-        Nombre: "",
-        Precio:"",
-        Stock:"",
-        Descripcion:"",
-        Imagen:""
-    },
-    {
-        ID:71,
-        Nombre: "",
-        Precio:"",
-        Stock:"",
-        Descripcion:"",
-        Imagen:""
-    },
-    {
-        ID:72,
-        Nombre: "",
-        Precio:"",
-        Stock:"",
-        Descripcion:"",
-        Imagen:""
-    },
-    {
-        ID:73,
-        Nombre: "",
-        Precio:"",
-        Stock:"",
-        Descripcion:"",
-        Imagen:""
-    },
-    {
-        ID:74,
-        Nombre: "",
-        Precio:"",
-        Stock:"",
-        Descripcion:"",
-        Imagen:""
-    },
-    {
-        ID:75,
-        Nombre: "",
-        Precio:"",
-        Stock:"",
-        Descripcion:"",
-        Imagen:""
-    },
-    {
-        ID:76,
-        Nombre: "",
-        Precio:"",
-        Stock:"",
-        Descripcion:"",
-        Imagen:""
-    },
-    {
-        ID:77,
-        Nombre: "",
-        Precio:"",
-        Stock:"",
-        Descripcion:"",
-        Imagen:""
-    },
-    {
-        ID:78,
-        Nombre: "",
-        Precio:"",
-        Stock:"",
-        Descripcion:"",
-        Imagen:""
-    },
-    {
-        ID:79,
-        Nombre: "",
-        Precio:"",
-        Stock:"",
-        Descripcion:"",
-        Imagen:""
-    },
-    {
-        ID:80,
-        Nombre: "",
-        Precio:"",
-        Stock:"",
-        Descripcion:"",
-        Imagen:""
-    },
-    {
-        ID:81,
-        Nombre: "",
-        Precio:"",
-        Stock:"",
-        Descripcion:"",
-        Imagen:""
-    },
-    {
-        ID:82,
-        Nombre: "",
-        Precio:"",
-        Stock:"",
-        Descripcion:"",
-        Imagen:""
-    },
-    {
-        ID:83,
-        Nombre: "",
-        Precio:"",
-        Stock:"",
-        Descripcion:"",
-        Imagen:""
-    },
-    {
-        ID:84,
-        Nombre: "",
-        Precio:"",
-        Stock:"",
-        Descripcion:"",
-        Imagen:""
-    },
-    {
-        ID:85,
-        Nombre: "",
-        Precio:"",
-        Stock:"",
-        Descripcion:"",
-        Imagen:""
-    },
-    {
-        ID:86,
-        Nombre: "",
-        Precio:"",
-        Stock:"",
-        Descripcion:"",
-        Imagen:""
-    },
-    {
-        ID:87,
-        Nombre: "",
-        Precio:"",
-        Stock:"",
-        Descripcion:"",
-        Imagen:""
-    },
-    {
-        ID:88,
-        Nombre: "",
-        Precio:"",
-        Stock:"",
-        Descripcion:"",
-        Imagen:""
-    },
-    {
-        ID:89,
-        Nombre: "",
-        Precio:"",
-        Stock:"",
-        Descripcion:"",
-        Imagen:""
-    },
-    {
-        ID:90,
-        Nombre: "",
-        Precio:"",
-        Stock:"",
-        Descripcion:"",
-        Imagen:""
-    },
-    {
-        ID:91,
-        Nombre: "",
-        Precio:"",
-        Stock:"",
-        Descripcion:"",
-        Imagen:""
-    },
-    {
-        ID:92,
-        Nombre: "",
-        Precio:"",
-        Stock:"",
-        Descripcion:"",
-        Imagen:""
-    },
-    {
-        ID:93,
-        Nombre: "",
-        Precio:"",
-        Stock:"",
-        Descripcion:"",
-        Imagen:""
-    },
-    {
-        ID:94,
-        Nombre: "",
-        Precio:"",
-        Stock:"",
-        Descripcion:"",
-        Imagen:""
-    },
-    {
-        ID:95,
-        Nombre: "",
-        Precio:"",
-        Stock:"",
-        Descripcion:"",
-        Imagen:""
-    },
-    {
-        ID:96,
-        Nombre: "",
-        Precio:"",
-        Stock:"",
-        Descripcion:"",
-        Imagen:""
-    },
-    {
-        ID:97,
-        Nombre: "",
-        Precio:"",
-        Stock:"",
-        Descripcion:"",
-        Imagen:""
-    },
-    {
-        ID:98,
-        Nombre: "",
-        Precio:"",
-        Stock:"",
-        Descripcion:"",
-        Imagen:""
-    },
-    {
-        ID:99,
-        Nombre: "",
-        Precio:"",
-        Stock:"",
-        Descripcion:"",
-        Imagen:""
-    },
-    {
-        ID:100,
-        Nombre: "",
-        Precio:"",
-        Stock:"",
-        Descripcion:"",
-        Imagen:""
-    },
-
+    }
 ]
---
+++ script/carrito.js
@@ -0,0 +1,7 @@
+window.cargarcarrito = function() {
+    Swal.fire({
+        title: "The producto is a enter in the changuito",
+        icon: "success",
+        draggable: true
+    });
+}
